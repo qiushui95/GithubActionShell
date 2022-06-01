ce83683d4b87c9610c7d156a32ad1b3698435b92
Ycx-Company <974577817@qq.com>
2022-06-01 11:03:22 +0800

    更新version获取脚本

--- env_version.sh
+++ env_version.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$2=$(cat $1 | grep -Po '\d+\.\d+\.\d+')">> $GITHUB_ENV
+cat $1 | grep -Po 'VERSION_NAME=\d+\.\d+\.\d+\S*' >>temp_grep.txt & cat temp_grep.txt | grep -Po '\d+\.\d+\.\d+\S*'>>version_grep.txt & rm temp_grep.txt &echo "$2=$(cat version_grep.txt)" >>$GITHUB_ENV &rm version_grep.txt
