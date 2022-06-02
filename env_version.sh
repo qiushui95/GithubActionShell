d0eaf90fb084b5fa32cb7173535430a6494c950d
Ycx-Company <974577817@qq.com>
2022-06-02 11:39:31 +0800

    fix version

--- env_version.sh
+++ env_version.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cat $1 | grep -Po 'VERSION_NAME=\d+\.\d+\.\d+\S*' >>temp_grep.txt & cat temp_grep.txt | grep -Po '\d+\.\d+\.\d+\S*'>>version_grep.txt & rm temp_grep.txt &echo "$2=$(cat version_grep.txt)" >>$GITHUB_ENV &rm version_grep.txt
+version_line=$(cat $1 | grep -Po 'VERSION_NAME=\d+\.\d+\.\d+\S*')
+version=$(echo $version_line | grep -Po '\d+\.\d+\.\d+\S*')
+echo "$2=$version"
